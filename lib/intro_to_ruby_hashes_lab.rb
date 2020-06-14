8471575f33a0dcccad64ddfefa62e3b4ffff33fd
Jennifer Grenier <jenniferagrenier@gmail.com>
2020-06-14 22:17:23 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,5 +1,5 @@
 def my_hash_creator(key, value)
-  {name: "George"}
+  {name: "Grace Hooper"}
   # return a hash that includes the key and value parameters passed into this method
 end
 
@@ -9,6 +9,6 @@ end
 
 def update_counting_hash(hash, key)
   # given a hash an a key as parameters, return an updated hash
-  # if the provided key is not present in the hash, add it and assign it to the value of 1
+  # if the provided key is not present in the h ash, add it and assign it to the value of 1
   # if the provided key is present, increment its value by 1
 end
